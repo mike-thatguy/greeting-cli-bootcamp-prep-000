6bc0e2fb0055906160bfec203b9308fbfc1deb6f
Mike Gretton <mikegretton@gmail.com>
2020-04-20 16:13:22 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-  puts "Greeting and salutations, #{name}!!"
+  puts "Hello #{name}. It's nice to meet you."
 end
 
 puts "Please enter your name!"
